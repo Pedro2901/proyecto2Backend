3fd8203534ea0b14f279eb806fd6c5a91f63e02e
pedroJimenez <ppjimenez@uninorte.edu.co>
2023-05-22 19:06:25 -0500

    medio
    medio

--- middlewares/authMiddle.js
+++ middlewares/authMiddle.js
@@ -1,23 +1,23 @@
 import jwt from 'jsonwebtoken';
 
-// export function verifyToken(authHeader) {
-//     try {
-//         const deco = jwt.verify(authHeader, process.env.JWT_SECRET);
-//         return deco;
-//     } catch (error) { return error; }
-// }
+export function verifyToken(authHeader) {
+    try {
+        const deco = jwt.verify(authHeader, process.env.JWT_SECRET);
+        return deco;
+    } catch (error) { return error; }
+}
 
-// export async function authWithToken(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) return res.status(401).json({ error: 'El token de autorización es requerido' });
+export async function authWithToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ error: 'El token de autorización es requerido' });
 
-//     const token = authHeader.split(' ')[1];
-//     if (!token) return res.status(401).json({ error: 'No ha sido proveído un token de autorización' });
+    const token = authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ error: 'No ha sido proveído un token de autorización' });
 
-//     const decoded = verifyToken(token);
-//     if (!decoded) return res.status(401).json({ error: 'Token inválido' });
-//     if (decoded instanceof Error) return res.status(401).json({ error: 'Token inválido' });
+    const decoded = verifyToken(token);
+    if (!decoded) return res.status(401).json({ error: 'Token inválido' });
+    if (decoded instanceof Error) return res.status(401).json({ error: 'Token inválido' });
 
-//     req.idUsuario = decoded.sub
-//     next();
-// }
+    req.idUsuario = decoded.sub
+    next();
+}
