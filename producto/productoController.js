bd14a268aceb2bd0ae579ccbe6e530b6f5596758
Yassg85 <123429815+Yassg85@users.noreply.github.com>
2023-05-26 11:45:39 -0500

    Update productoController.js

--- producto/productoController.js
+++ producto/productoController.js
@@ -20,6 +20,29 @@ export const createProduct = async (req, res) => {
   };
 
   export const getProducts=async(req,res)=>{
+    // Importar los módulos necesarios
+const express = require('express');
+const app = express();
+
+// Datos de ejemplo (puedes reemplazarlo con tu propia lógica para obtener los productos)
+const productos = [
+  { id: 1, nombre: 'Producto 1', precio: 10.99 },
+  { id: 2, nombre: 'Producto 2', precio: 15.99 },
+  { id: 3, nombre: 'Producto 3', precio: 7.99 },
+];
+
+// Ruta GET para obtener todos los productos
+app.get('/productos', (req, res) => {
+  // Aquí puedes realizar cualquier lógica adicional, como consultar una base de datos
+
+  // Enviar los productos como respuesta
+  res.json(productos);
+});
+
+// Iniciar el servidor en el puerto deseado
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
 
   };
 
@@ -28,10 +51,48 @@ export const createProduct = async (req, res) => {
   }
 
   export const updateProductById=async(req,res)=>{
-
+    app.put('/productos/:id', (req, res) => {
+      // Obtener el ID del producto de los parámetros de la URL
+      const productId = req.params.id;
+    
+      // Obtener los nuevos datos del producto del cuerpo de la solicitud
+      const { nombre, precio } = req.body;
+    
+      // Aquí puedes realizar cualquier lógica adicional, como verificar la validez de los datos o actualizar la base de datos
+    
+      // Actualizar el producto con los nuevos datos
+      for (let i = 0; i < productos.length; i++) {
+        if (productos[i].id === parseInt(productId)) {
+          productos[i].nombre = nombre;
+          productos[i].precio = precio;
+          break;
+        }
+      }
+    
+      // Enviar una respuesta indicando éxito
+      res.json({ mensaje: 'Producto actualizado con éxito' });
+    });
   }
 
   export const deleteProductById=async(req,res)=>{
+    // Ruta DELETE para eliminar un producto
+app.delete('/productos/:id', (req, res) => {
+  // Obtener el ID del producto de los parámetros de la URL
+  const productId = req.params.id;
 
+  // Aquí puedes realizar cualquier lógica adicional, como verificar la validez del ID o eliminar el producto de la base de datos
+
+  // Buscar el índice del producto en el arreglo
+  const productIndex = productos.findIndex(producto => producto.id === parseInt(productId));
+
+  // Verificar si se encontró el producto
+  if (productIndex !== -1) {
+    // Eliminar el producto del arreglo
+    productos.splice(productIndex, 1);
+    res.json({ mensaje: 'Producto eliminado con éxito' });
+  } else {
+    res.status(404).json({ mensaje: 'Producto no encontrado' });
+  }
+});
   }
 
